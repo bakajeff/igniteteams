d2b68d8af1b6a8c4a8e971ae5967b4b5b9a02104
jeffte costa <jefftewf17@outlook.com>
2023-01-20 20:14:14 -0300

    feat: add Players screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { NewGroup } from "@screens/NewGroup";
+import { Players } from "@screens/Players";
 import { ThemeProvider } from "styled-components";
 
 import theme from "@theme/index";
@@ -22,7 +22,7 @@ export default function App() {
 				backgroundColor="transparent"
 				translucent
 			/>
-			{fontsLoaded ? <NewGroup /> : <Loading />}
+			{fontsLoaded ? <Players /> : <Loading />}
 		</ThemeProvider>
 	);
 }
