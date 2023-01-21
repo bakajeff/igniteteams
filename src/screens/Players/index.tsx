127e5183d07e271d5efca0d20245176e5a053dae
jeffte costa <jefftewf17@outlook.com>
2023-01-21 19:01:57 -0300

    feat: add Filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
@@ -15,9 +16,10 @@ export function Players() {
 			/>
 			<Form>
 				<Input placeholder="Nome da pessoa" autoCorrect={false} />
-
 				<ButtonIcon icon="add" />
 			</Form>
+
+			<Filter title="Time A" isActive />
 		</Container>
 	);
 }
