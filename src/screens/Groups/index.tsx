7b37cd19a43187d1b37bae74146c3a1abe5522be
jeffte costa <jefftewf17@outlook.com>
2023-01-22 17:34:13 -0300

    refactor: hide vertical scroll indicator

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -27,6 +27,7 @@ export function Groups() {
 				ListEmptyComponent={() => (
 					<ListEmpty message="Que tal cadastrar a primeira turma?" />
 				)}
+				showsVerticalScrollIndicator={false}
 			/>
 
 			<Button title="Criar nova turma" />
