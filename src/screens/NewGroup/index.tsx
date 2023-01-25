41d62c99b377aca95532d6fe113b53a89a3ba338
jeffte costa <jefftewf17@outlook.com>
2023-01-25 14:25:54 -0300

    feat: implements handleNew function

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,14 +6,18 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
+import { groupCreate } from "@storage/group/groupCreate";
 
 export function NewGroup() {
 	const [group, setGroup] = useState("");
 
 	const navigation = useNavigation();
 
-	function handleNew() {
-		navigation.navigate("players", { group });
+	async function handleNew() {
+		try {
+			await groupCreate(group);
+			navigation.navigate("players", { group });
+		} catch (error) {}
 	}
 
 	return (
