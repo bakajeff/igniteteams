b51536539efa77e24cdb10680993aa3c20053123
jeffte costa <jefftewf17@outlook.com>
2023-01-25 14:48:18 -0300

    feat: verify if group already exists

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+
 import { Container, Content, Icon } from "./styles";
+
 import { groupCreate } from "@storage/group/groupCreate";
+
+import { AppError } from "@utils/AppError";
 
 export function NewGroup() {
 	const [group, setGroup] = useState("");
@@ -17,7 +22,14 @@ export function NewGroup() {
 		try {
 			await groupCreate(group);
 			navigation.navigate("players", { group });
-		} catch (error) {}
+		} catch (error) {
+			if (error instanceof AppError) {
+				Alert.alert("Novo Grupo", error.message);
+			} else {
+				Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo");
+				console.log(error);
+			}
+		}
 	}
 
 	return (
