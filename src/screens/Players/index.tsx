5f5b1ce5d4f5c7ed512276007e6eee5f09e2a8d3
jeffte costa <jefftewf17@outlook.com>
2023-01-26 15:55:20 -0300

    feat: remove player by group

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -16,6 +16,7 @@ import { AppError } from "@utils/AppError";
 import { playerAddByGroup } from "@storage/player/playerAddByGroup";
 import { playersGetByGroupAndTeam } from "@storage/player/playersGetByGroupAndTeam";
 import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
+import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
 
 type RouteParams = {
 	group: string;
@@ -74,6 +75,16 @@ export function Players() {
 		}
 	}
 
+	async function handleRemovePlayer(playerName: string) {
+		try {
+			await playerRemoveByGroup(playerName, group);
+			fetchPlayersByTeam();
+		} catch (error) {
+			console.log(error);
+			Alert.alert("Remover pessoa", "Não foi possível remover essa pessoa");
+		}
+	}
+
 	useEffect(() => {
 		fetchPlayersByTeam();
 	}, [team]);
@@ -116,7 +127,10 @@ export function Players() {
 				data={players}
 				keyExtractor={(item) => item.name}
 				renderItem={({ item }) => (
-					<PlayerCard name={item.name} onRemove={() => {}} />
+					<PlayerCard
+						name={item.name}
+						onRemove={() => handleRemovePlayer(item.name)}
+					/>
 				)}
 				ListEmptyComponent={() => (
 					<ListEmpty message="Não há pessoas nesse time." />
